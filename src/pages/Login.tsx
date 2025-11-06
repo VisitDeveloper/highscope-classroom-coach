42176da5ac828922c0a07aa7eeb5715f5d79e920
fire_full <amiralihatami220@gmail.com>
2025-11-06 13:35:18 +0200

    add something changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,13 +20,13 @@ function Login() {
       key: 0,
       colSpan: { xs: 24, md: 24, lg: 24 },
       rules: [
-        { type: 'text', message: 'Please insert string into the input' },
+        { type: 'email', message: 'Please insert email into the input' },
         { required: true, message: 'User Name is required!' },
-        { min: 2, message: 'Minimum 2 characters' },
-        { max: 100, message: 'Maximum 100 characters' },
+        { max: 200, message: 'Maximum 100 characters' },
         { whitespace: true, message: 'No space allowed' },
 
       ],
+      hasFeedback: true,
       propsElement: {
         placeholder: 'User Name',
         style: {
@@ -58,23 +58,24 @@ function Login() {
     <div
       className='w-full h-screen object-cover bg-center bg-cover bg-no-repeat'
       style={{
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('/images/bg-login-1.jpg')`,
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(255,255,255,0.7)), url('/images/bg-login-1.jpg')`,
         backdropFilter: 'blur(5px)', // برای blur
       }}
     >
-      <div className='flex flex-row justify-center items-center h-full'>
-        <div className="w-[30%]! h-fit rounded-sm py-5!" style={{ backgroundColor: colorBgBase }}>
-          <div className="flex flex-col px-9! gap-2">
-            <div className="mt-5!">
-              <ClassRoomLogoComponent width={"100%"} height={"100"} />
+      <div className='flex flex-row justify-center items-center h-full '>
+        <div className="w-[90%] sm:w-[70%]! md:w-[50%]! lg:w-[40%]! xl:w-[30%]! h-fit rounded-sm py-3! " style={{ backgroundColor: '#fff' }}>
+            <div className="mt-5! text-center">
+              {/* <ClassRoomLogoComponent width={"100%"} height={"100"} /> */}
+              <img src="/images/bg-logo.jpg" style={{width:'100%',height:'auto'}} />
             </div>
+          <div className="flex flex-col px-9! gap-2">
             <div >
               <Typography.Title level={4} className="text-center inter-font" style={{ color: '#333' }}>
                 Sign In
               </Typography.Title>
 
               <Typography.Title level={5} className="inter-font" style={{ color: '#333' }}>
-                Sign in with your username or email address
+                Sign in with your username or email 
               </Typography.Title>
             </div>
             <div className="">
--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -1,20 +1,34 @@
 import { Button, Typography } from "antd";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 function Notfound() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div className="flex flex-col gap-5 justify-center items-center text-2xl h-[60vh]" >
+    <div className="not-found">
+      {/* <div className="flex flex-col gap-5 justify-center items-center text-2xl h-[60vh]" >
         <Typography.Title level={2} className="m-0" >
           404 - Page Not Found
         </Typography.Title>
         <Button type="link" onClick={() => navigate('/')}>
           Go to Home Page
         </Button>
+      </div> */}
+      <div className="container">
+        <h1 className="font-black h1">404</h1>
+        <h2 className="h2">Page Not Found</h2>
+        <p>Oops! The page you're looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+        <div className="illustration-placeholder">
+          <img src="/images/not.png" alt="404 Illustration"  />
+        </div>
+        <p className="p">Please check the URL or go back to the homepage.</p>
+        <Link to={'/'} className="a">
+          Go to Homepage
+        </Link>
       </div>
+
     </div>
   )
 }
-export default Notfound
+export default Notfound;
+
